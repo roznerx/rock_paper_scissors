a0acb2fec62638d7de4a08710ff5bab28b334e43
roznerx <ignaciorosner@gmail.com>
2021-05-13 16:53:54 -0300

    Tasks for the current challenge completedgit add .! Notes added regarding some stuff that could be fixed to make the code and the overall result more satisfactory

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -23,7 +23,7 @@ const playerSelection = playerChoice();
 
 //1.2.Computer's choices
 
-function computerPlay(){
+function computerPlay() {
 
     let random = ["rock", "paper", "scissors"];
     return random[Math.floor(Math.random() * 3)];
@@ -54,99 +54,63 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "scissors") {
             console.log(winMessage + rockBeats);
             playerScore += 1;
-            let winner = "Player";
+            winner = "Player";
         } else if (computerSelection === "paper") {
             console.log(loseMessage + paperBeats);
             computerScore += 1;
-            let winner = "Computer";
+            winner = "Computer";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             console.log(winMessage + paperBeats);
             playerScore += 1;
-            let winner = "Player";
+            winner = "Player";
         } else if (computerSelection === "scissors") {
             console.log(loseMessage + scissorsBeat);
             computerScore += 1;
-            let winner = "Computer";
+            winner = "Computer";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
             console.log(winMessage + scissorsBeat);
             playerScore += 1;
-            let winner = "Player";
+            winner = "Player";
         } else if (computerSelection === "rock") {
             console.log(loseMessage + rockBeats);
             computerScore += 1;
-            let winner = "Computer";
+            winner = "Computer";
         }
     } 
 
         if (winner = "Player"||"Computer") {
-        while (playerScore < 5 && computerScore < 5) {
-            playerChoice();
-            computerPlay();
-            playRound(playerSelection, computerSelection);
-            if (playerScore = 5) {
-                console.log("Player victorious! Machine is crushed!");
-            } else if (computerScore = 5) {
-                console.log("Game over! Kneel before me, you filthy human!");
-            }
+            console.log("Player: " + playerScore + " - " + "Computer: " + computerScore);
+            while (playerScore < 5 && computerScore < 5) {
+                let newPlayerChoice = playerChoice();
+                let newComputerPlay = computerPlay();
+                playRound(newPlayerChoice, newComputerPlay);
+                if (playerScore = 5 && computerScore < playerScore) {
+                    console.log("Player victorious! Machine is crushed!");
+                } else if (computerScore = 5 && playerScore < computerScore) {
+                    console.log("Game over! Kneel before me, you filthy human!");
+                }
+                break
+            } 
         }
-    }           
 }
-
 
 console.log(playRound(playerSelection, computerSelection));
 console.log("Player: " + playerScore + " - " + "Computer: " + computerScore);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Press F5 to play again!");
 
 /*
 
-function game() {
+Some notes regarding the current version!!!
 
-    while (playerScore < 5 && computerScore < 5) {
-        
-    }
+-Game is up and running!
 
+-Couple of things to fix:
 
+1) When the game ends, console displays console.log as "undefined" and "Player: true - Computer: false" strings.
+2) When the prompt happens at the beginning of rounds after the 1st one, no "Invalid input!" message happens.
 
-}
-
-
-
-
-function gameLoop() {
-
-    while (playerScore < 5 && computerScore < 5) {
-        playRound(playerSelection, computerSelection)
-        
-        
-    }
-
-        if (playerScore = 5) {
-            return "You win! Man beats machine!"  
-        } else if (computerScore = 5) {
-            return "You lose! Pathetic human!"
-        }
-    
-}
 */
